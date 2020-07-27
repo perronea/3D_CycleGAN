f21672c7faa5020df780f6699509c299e87b8fe5
perronea <perronea@metal-1-0.local>
2020-07-27 16:50:40 -0700

    Fix A test path

--- load_data.py
+++ load_data.py
@@ -6,20 +6,22 @@ from keras.utils import Sequence
 #from skimage.io import imread
 
 
-def load_data(image_folder, input_modality, nr_of_channels, batch_size=1, 
+def load_data(nr_of_channels, batch_size=1, 
               nr_A_train_imgs=None, nr_B_train_imgs=None,
               nr_A_test_imgs=None, nr_B_test_imgs=None,
-              generator=False, D_model=None, use_multiscale_discriminator=False, use_supervised_learning=False, REAL_LABEL=1.0):
+              generator=False, D_model=None, use_multiscale_discriminator=False, use_supervised_learning=False, REAL_LABEL=1.0, subfolder='MR_3D'):
 
-    train_A_path = os.path.join(image_folder, input_modality)
-    train_B_path = os.path.join(image_folder, input_modality)
-    test_A_path = os.path.join(image_folder, input_modality)
-    test_B_path = os.path.join(image_folder, input_modality)
+    train_A_path = os.path.join('data', subfolder, 'trainA')
+    train_B_path = os.path.join('data', subfolder, 'trainB')
+    test_A_path = os.path.join('data', subfolder, 'testA')
+    test_B_path = os.path.join('data', subfolder, 'testB')
 
     train_A_image_names = os.listdir(train_A_path)
     train_B_image_names = os.listdir(train_B_path)
     test_A_image_names = os.listdir(test_A_path)
     test_B_image_names = os.listdir(test_B_path)
+
+    print(train_A_image_names)
 
 
     if generator:
@@ -31,7 +33,7 @@ def load_data(image_folder, input_modality, nr_of_channels, batch_size=1,
         test_B_images = create_image_array(test_B_image_names, test_B_path, nr_of_channels)
         return {"train_A_images": train_A_images,  "train_A_image_names": train_A_image_names,
                 "train_B_images": train_B_images,  "train_B_image_names": train_B_image_names,
-                "test_A_images": train_A_images,  "test_A_image_names": train_A_image_names,
+                "test_A_images": test_A_images,  "test_A_image_names": test_A_image_names,
                 "test_B_images": test_B_images,  "test_B_image_names": test_B_image_names,
 
 }
@@ -40,18 +42,24 @@ def load_data(image_folder, input_modality, nr_of_channels, batch_size=1,
 def create_image_array(image_list, image_path, nr_of_channels):
     image_array = []
     for image_name in image_list:
-        if image_name[-1].lower() == 'g':  # to avoid e.g. thumbs.db files
-            if nr_of_channels == 1:  # Gray scale image -> MR image
-                image = np.array(Image.open(os.path.join(image_path, image_name)))
-                img = nib.load(os.path.join(image_path, image_name))
-                image = img.get_fdata()
-                image = image[:, :, :, np.newaxis]
-            else:                   # RGB image -> 3 channels
-                image = np.array(Image.open(os.path.join(image_path, image_name)))
-                img = nib.load(os.path.join(image_path, image_name))
-                image = img.get_fdata()
-            image = normalize_array(image)
-            image_array.append(image)
+        print(image_name)
+        #if image_name[-1].lower() == 'g':  # to avoid e.g. thumbs.db files
+        if nr_of_channels == 1:  # Gray scale image -> MR image
+            #image = np.array(Image.open(os.path.join(image_path, image_name)))
+            img = nib.load(os.path.join(image_path, image_name))
+            image = img.get_fdata()
+            image = np.pad(image,((1,0),(1,2),(1,0)), mode='constant')
+
+            image = image[:, :, :, np.newaxis]
+
+        else:                   # RGB image -> 3 channels
+            #image = np.array(Image.open(os.path.join(image_path, image_name)))
+            print("loading image_name")
+            img = nib.load(os.path.join(image_path, image_name))
+            image = img.get_fdata()
+            image = np.pad(image,((1,0),(1,2),(1,0)), mode='constant')
+        image = normalize_array(image)
+        image_array.append(image)
 
     return np.array(image_array)
   
